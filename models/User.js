37793efc962bad906c260635e0574a329e8fea5f
Flow-Fly <florian.aube@gmail.com>
2021-07-30 10:42:01 +0200

    user model

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userModel = new Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required:true,
+        unique: true
+    },
+    password: String
+    })
+
+const User = mongoose.model('user', userModel)
+
+module.exports(User)
